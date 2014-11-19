d7706ac01d7b0b714c422eda320802d2612394e8
Nikolay Sadovnikov <gruzzilkin@gmail.com>
2014-11-19 22:08:28 +0500

    Add unit-tests and some refactoring

--- common.blocks/cut/cut.deps.js
+++ common.blocks/cut/cut.deps.js
@@ -1,9 +1,23 @@
 [{
     mustDeps : [
-        { block : 'i-bem', elem : 'dom' }
+        { block : 'i-bem', mods : { 'elem-instances' : true }, elem : 'dom' }
     ],
     shouldDeps : [
         { elems : ['switcher', 'container'] },
         { block : 'link', mods : { theme : 'islands', pseudo : true } }
+    ]
+},
+{
+    tech : 'spec.js',
+    mustDeps : [
+        { tech : 'bemhtml', block : 'cut', mods : { theme : 'hackaton' } },
+        { block : 'link', mods : { theme : 'islands', type : 'pseudo' } }
+    ]
+},
+{
+    tech : 'tmpl-spec.js',
+    mustDeps : [
+        { tech : 'bemhtml', block : 'cut', mods : { theme : 'hackaton' } },
+        { block : 'link', mods : { theme : 'islands', type : 'pseudo' } }
     ]
 }]
